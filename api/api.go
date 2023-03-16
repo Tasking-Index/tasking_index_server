c5c7c7c6a67a686ac88afd82af7c25f495d98e36
David Pastor Crespo <davidgasol913@gmail.com>
2023-03-16 16:28:23 +0100

    Creación Proyecto DONE

--- api/api.go
+++ api/api.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/sha256"
 	"encoding/json"
 	"io"
 	"net/http"
@@ -12,11 +13,16 @@ type User struct {
 	Password string `json:"pass"`
 }
 
-type project struct {
-	Id          int
-	Titulo      string
-	Descripcion string
-	Archivos    []string
+type UsersProjects struct {
+	Users    map[string][]int `json:"users"`
+	Projects []Project        `json:"projects"`
+}
+
+type Project struct {
+	Id          int      `json:"id"`
+	Title       string   `json:"title"`
+	Description string   `json:"description"`
+	Files       []string `json:"files"`
 }
 
 type additional struct {
@@ -48,7 +54,113 @@ func mailExists(users map[string]string, user User, needPassword bool) bool {
 	return ok
 }
 
-// Falta guardar las contraseñas hasheadas
+func hash(s string) []byte {
+	hasher := sha256.New()
+	hasher.Write([]byte(s))
+	return hasher.Sum(nil)
+}
+
+// Maps users.json
+func mapUserJson() map[string]string {
+	data, fileErr := os.ReadFile("../data/users.json")
+	check(fileErr)
+	users := make(map[string]string)
+	error := json.Unmarshal(data, &users)
+	check(error)
+	return users
+}
+
+// Assigns body parameters to a user
+func getBodyUser(req *http.Request) User {
+	var user User
+	body, reqErr := io.ReadAll(req.Body)
+	check(reqErr)
+	error := json.Unmarshal(body, &user)
+	check(error)
+	return user
+}
+
+// TODO
+func projectsMatched(userId string) {
+	//Save json to struct
+	var usersProjects UsersProjects
+	data, fileErr := os.ReadFile("../data/projects.json")
+	check(fileErr)
+	json.Unmarshal(data, &usersProjects)
+}
+
+// Request {"id": 1, "user":"paco", "pass": "paco1"}
+/*
+func getProject(w http.ResponseWriter, req *http.Request) {
+	body, reqErr := io.ReadAll(req.Body)
+	check(reqErr)
+	users := mapUserJson()
+	var user User
+	json.Unmarshal(body, &user)
+	if mailExists(users, user, true) {
+		//DAR INFORMACIÓN DEL PROYECTO
+		print("EXISTE")
+		//var projects []Project
+
+	} else {
+		resp := make(map[string]string)
+		resp["msg"] = "Login needed to get projects"
+		jsonResp, respErr := json.Marshal(resp)
+		check(respErr)
+		w.WriteHeader(400)
+		w.Write(jsonResp)
+	}
+}
+*/
+
+// Request: {"user":"user1", "pass":"pass1", "title": "Title Project"}
+// TODO
+func createProject(w http.ResponseWriter, req *http.Request) {
+	body, reqErr := io.ReadAll(req.Body)
+	check(reqErr)
+	users := mapUserJson()
+	var user User
+	json.Unmarshal(body, &user)
+	if mailExists(users, user, true) {
+		bodyInfo := make(map[string]string)
+		json.Unmarshal(body, &bodyInfo)
+		data, fileErr := os.ReadFile("../data/projects.json")
+		check(fileErr)
+		var usersProjects UsersProjects
+		var project Project
+		json.Unmarshal(data, &usersProjects)
+		lastItem := len(usersProjects.Projects)
+		if lastItem == 0 {
+			project.Id = 0
+		} else {
+			project.Id = usersProjects.Projects[lastItem-1].Id + 1
+		}
+		project.Title = bodyInfo["title"]
+		project.Description = ""
+		project.Files = nil
+		usersProjects.Projects = append(usersProjects.Projects, project)
+		usersProjectsJSON, JsonErr := json.MarshalIndent(usersProjects, "", "  ")
+		check(JsonErr)
+		erro := os.WriteFile("../data/projects.json", usersProjectsJSON, 0666)
+		check(erro)
+		resp := make(map[string]interface{})
+		resp["msg"] = "Project correctly created"
+		resp["id"] = project.Id
+		jsonResp, respErr := json.Marshal(resp)
+		check(respErr)
+		w.WriteHeader(200)
+		w.Write(jsonResp)
+	} else {
+		resp := make(map[string]string)
+		resp["msg"] = "Login needed to create a project"
+		jsonResp, respErr := json.Marshal(resp)
+		check(respErr)
+		w.WriteHeader(400)
+		w.Write(jsonResp)
+	}
+}
+
+// TODO Hash passwords
 func register(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var user User
@@ -85,34 +197,12 @@ func register(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
-// Maps users.json
-func mapUserJson() map[string]string {
-	data, fileErr := os.ReadFile("../data/users.json")
-	check(fileErr)
-	users := make(map[string]string)
-	json.Unmarshal(data, &users)
-	return users
-}
-
-// Assigns body parameters to a user
-func getBodyUser(req *http.Request) User {
-	var user User
-	body, reqErr := io.ReadAll(req.Body)
-	check(reqErr)
-	json.Unmarshal(body, &user)
-	return user
-}
-
-// Falta adjuntar la clave
+// TODO Change map[string]string to map[string][]byte (hash) and send projects
 func login(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var user User
 	user = getBodyUser(req)
 	users := mapUserJson()
-	/*
-		hash := sha256.New()
-		hash.Write([]byte(user.Password))
-	*/
 	if mailExists(users, user, true) {
 		resp := make(map[string]string)
 		resp["msg"] = "User correctly logged"
@@ -133,6 +223,8 @@ func login(w http.ResponseWriter, req *http.Request) {
 func main() {
 	http.HandleFunc("/register", register)
 	http.HandleFunc("/login", login)
+	http.HandleFunc("/createProject", createProject)
+	//http.HandleFunc("/getProject", getProject)
 	err := http.ListenAndServe("localhost:443", nil)
 	check(err)
 }
